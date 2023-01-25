f5deec194aef238be45ebd38eae5c5487594fca0
BrianJ Kim <brianjkim.dev@google.com>
2023-01-25 15:08:37 -0600

    Change modal window size

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,10 +11,10 @@ export default ({children, isOpen, toggle}: ModalType) => {
   return (
     <>
       {isOpen && (
-        <div className="flex justify-center items-center bg-black/[.55] fixed top-0 w-full h-screen z-20 overscroll-none" onClick={toggle}>
+        <div className="flex justify-center items-center bg-black/[.55] fixed top-0 w-full h-screen z-20 " onClick={toggle}>
           <div 
             onClick={(e) => e.stopPropagation()}
-            className="block bg-white p-3 rounded-md w-3/4 h-[360px]"
+            className="block bg-white p-3 rounded-md w-[480px] h-[380px]"
           >
             {children}
           </div>
